e0ab2d037fb44b6619a29c3d09a78b64402d62fd
daballard1127 <denise_ballard@att.net>
2017-11-29 18:14:54 -0600

    Final Updates to Homework 11-29-2017

--- liri.js
+++ liri.js
@@ -5,6 +5,7 @@ var spotify = require("Spotify");
 var dbkey = require("./keys.js");
 var callTweets = require("./twitter");
 var callMovieName = require("./movie");
+var callSong = require("./spotify");
 var fs = require("fs");
 
 function takingaCommand() {
@@ -15,16 +16,29 @@ function takingaCommand() {
 		callTweets();
 		console.log("tweety bird");
 	} else if (command === "spotify-this-song") {
+		callSong();
 		console.log("sing to me");
 	} else if (command === "movie-this") {
 		callMovieName();
 		console.log("watching a movie")
 	} else if (command === "do-what-it-says") {
+		doWhatItSays();
 		console.log("Party all the time")
 	} else {
 		console.log("Done");
 	}
 }
 takingaCommand();
+
+function doWhatItSays() {
+		fs.readFile("random.txt", "utf8", function(error, data){
+			if (!error) {
+				doWhatItSaysResults = data.split(",");
+				callSong(doWhatItSaysResults[0], doWhatItSaysResults[1]);
+			} else {
+				console.log("Error occurred" + error);
+			}
+		});
+	};
 
 
--- spotify.js
+++ spotify.js
@@ -6,10 +6,31 @@ var spotify = new Spotify({
   id: '262577b127934aae91f04d0ca8eeafbe',
   secret: 'b05da352e6024e138d98f934b432e7cf'
 });
-var command = process.argv[2];
 
-if (process.argv[3]=== undefined){
- spotify.search({ type: 'track', query: "The Sign"}, function(err, data){
+function callSong(){
+// var command = process.argv[2];
+
+
+
+function returnSongName() {
+if (process.argv[3] === undefined) {
+	songSelection = "The Sign";
+	console.log(songSelection);
+	
+}else{
+	
+	for (var i = 3; i < process.argv.length; i++) {
+
+	var songSelection = "";
+		songSelection += process.argv[i] + " ";
+}
+	}
+	return songSelection;
+}
+var getSong = returnSongName();
+console.log(getSong);
+
+spotify.search({ type: 'track', query: getSong}, function(err, data){
                 var data = data.tracks.items;
                 console.log(data[0].name); //song track name
                 console.log(data[0].album.href); //url 
@@ -17,8 +38,7 @@ if (process.argv[3]=== undefined){
                 console.log(data[0].preview_url); //preview link to the song
                 console.log(data[0].artists[0].name); //artist's name
             });
-}else{
-	console.log(process.argv[3]);
-
 }
-return;
+
+callSong();
+module.exports = callSong;
